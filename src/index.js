3bd7e15b9207941c26b16d52d4566770d365001b
varsha960521104070 <varshanarayanan2003@gmail.com>
2024-10-29 13:41:19 +0530

    Files are added

--- src/index.js
+++ src/index.js
@@ -1,17 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import react from 'react';
+import reactDOM from 'react-dom/client';
+import App from './App.js'
+import './index.css'
+const root = reactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  <react.Fragment>
+    <div>
+    <h1>Hello World</h1>
+    <h2>Lets learn ReactJs</h2>
+  </div>
+  <App />
+  </react.Fragment>
+  
+  
+)
